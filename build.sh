99a4efb72b4443a3f8829c70ece811b8fce65ba2
y0unj1NoH <nohyoong711@gmail.com>
2024-11-28 01:31:14 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cd ../
-mkdir output
+# output 디렉토리 생성
+mkdir -p output
 cp -R ./WEB1_1_J1P5_FE/* ./output
+cp -R ./WEB1_1_J1P5_FE/.storybook ./output
+
 cp -R ./output ./WEB1_1_J1P5_FE/
+
+
+
