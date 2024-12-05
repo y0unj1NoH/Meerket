0ecfc222afc8dce38ec4c45d9c64bc227083d87e
y0unj1NoH <nohyoong711@gmail.com>
2024-12-05 18:56:04 +0000

    feat: MyPage 컴포넌트 개발 (response 타입 업데이트 필요 )

--- src/components/templates/MyPageTemplate/index.tsx
+++ src/components/templates/MyPageTemplate/index.tsx
@@ -8,7 +8,7 @@ interface IMyPageTemplateProps {
   /** 닉네임 */
   nickname: string;
   /** 인증한 지역 */
-  location: string;
+  address: string;
   /** 프로필 수정 버튼 클릭 시 실행될 함수 */
   onProfileEditButtonClick: () => void;
   /** 메뉴 클릭 시 실행될 함수 */
@@ -18,16 +18,15 @@ interface IMyPageTemplateProps {
 export const MyPageTemplate = ({
   imgUrl,
   nickname,
-  location,
+  address,
   onProfileEditButtonClick,
   onMenuClick
 }: IMyPageTemplateProps) => {
   return (
     <MyPageTemplateWrapper>
-      <Profile imgUrl={imgUrl} nickname={nickname} location={location} />
+      <Profile imgUrl={imgUrl} nickname={nickname} location={address} />
       <TextButton text="프로필 수정" onClick={onProfileEditButtonClick} />
       <MyPageMenu onMenuClick={onMenuClick} />
     </MyPageTemplateWrapper>
   );
 };
-
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,3 +1,68 @@
+import { useEffect, useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { MyPageTemplate } from "components/templates";
+import { http } from "services/api";
+import { IResponse } from "types";
+
+interface IProfile {
+  /** 프로필 이미지 URL */
+  imgUrl: string;
+  /** 닉네임 */
+  nickname: string;
+  /** 인증한 지역 */
+  address: string;
+}
+interface IProfileResponse extends IResponse {
+  result: IProfile;
+}
+
 export const MyPage = () => {
-  return <>MyPage</>;
+  const navigate = useNavigate();
+  const [profile, setProfile] = useState<IProfile>({
+    imgUrl: "",
+    nickname: "",
+    address: ""
+  });
+
+  const fetchProfile = useCallback(async () => {
+    try {
+      const response = await http.get<IProfileResponse>("/user/profile");
+      if (response.success && response.code === "COMMON200") {
+        setProfile(response.result);
+      } else {
+        console.error("Failed to fetch profile: Invalid response code");
+      }
+    } catch (error) {
+      console.error("Failed to fetch profile:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    /**
+     * fetchProfile에서 에러 catch하는데 여기서 또 할 필요가 없음
+     */
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchProfile();
+  }, [fetchProfile]);
+
+  const handleProfileEditButtonClick = useCallback(() => {
+    navigate("/profile/edit");
+  }, [navigate]);
+
+  const handleMenuClick = useCallback(
+    (pathname: string) => {
+      navigate(pathname);
+    },
+    [navigate]
+  );
+
+  return (
+    <MyPageTemplate
+      imgUrl={profile.imgUrl}
+      nickname={profile.nickname}
+      address={profile.address}
+      onProfileEditButtonClick={handleProfileEditButtonClick}
+      onMenuClick={handleMenuClick}
+    />
+  );
 };
