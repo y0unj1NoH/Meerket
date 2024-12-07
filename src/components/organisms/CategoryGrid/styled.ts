cc3fdf823180ecfa677c8503014eaba27d155960
y0unj1NoH <nohyoong711@gmail.com>
2024-12-07 09:26:55 +0000

    chore: 카테고리 디자인 반영

--- src/components/organisms/CategoryGrid/styled.ts
+++ src/components/organisms/CategoryGrid/styled.ts
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { ImageWrapper } from "components/atoms/Image/styled";
+import { ThemeType } from "styles/theme";
 
 export const CategoryGridWrapper: ReturnType<typeof styled.div> = styled.div`
   width: 100%;
@@ -12,4 +14,9 @@ export const CategoryItemWrapper: ReturnType<typeof styled.div> = styled.div`
   justify-content: center;
   align-items: flex-start;
   text-align: center;
+
+  ${ImageWrapper} {
+    border: 1px solid
+      ${({ theme }: { theme: ThemeType }) => theme.colors.blue_main};
+  }
 `;
--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,17 +1,25 @@
 import { ICategory } from "components/organisms/CategoryGrid";
 import { CategoryTemplate } from "components/templates";
 import { CATEGORIES } from "constants/categories";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useTopBarStore } from "stores";
 
 export const CategoryPage = () => {
+  const { clear, setTitle } = useTopBarStore();
+
   const navigate = useNavigate();
 
   const categories: ICategory[] = CATEGORIES.map((category) => ({
     title: category.name,
-    imgUrl: `/Default_Img.png`,
+    imgUrl: `/categorys/${category.code}.png`,
     serverType: category.code,
   }));
 
+  useEffect(() => {
+    clear();
+    setTitle("카테고리");
+  }, []);
   const handleCategoryButton = (category: string) => {
     navigate(`/search/category/${category}`);
   };
