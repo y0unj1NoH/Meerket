ef8cb8bbf7191710c5a0d4b2f2198b967236ea4a
y0unj1NoH <nohyoong711@gmail.com>
2024-12-08 18:47:37 +0000

    fix: RouterGuard 수정

--- src/pages/guards/AuthGuard.tsx
+++ src/pages/guards/AuthGuard.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useFetchSession } from "hooks";
+import { useUserStore } from "stores";
 
 /**
  * 로그인 되지 않은 사용자가 로그인이 필요한 페이지 접근 시 처리
  */
 export const AuthGuard = () => {
+  const { user } = useUserStore();
   const { sessionUser, isLoading } = useFetchSession();
 
   if (isLoading) {
@@ -15,11 +17,11 @@ export const AuthGuard = () => {
     return <Navigate to="/login" replace />;
   }
 
-  if (!sessionUser.nickname) {
+  if (!sessionUser.nickname && !user?.nickname) {
     return <Navigate to="/profile" replace />;
   }
 
-  if (!sessionUser.ActivityEmdName) {
+  if (!sessionUser.ActivityEmdName && !user?.emdId) {
     return <Navigate to="/neighborhood-selection" replace />;
   }
 
