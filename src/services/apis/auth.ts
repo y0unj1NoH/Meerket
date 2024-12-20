bdd3c4704de4b0f76af5b06861ba5a77dae115b8
y0unj1NoH <nohyoong711@gmail.com>
2024-12-20 09:30:08 +0000

    test: PWA Builder 주소 표시줄 테스트

--- src/services/apis/auth.ts
+++ src/services/apis/auth.ts
@@ -3,8 +3,12 @@ import type { IAuthData, IAuthResponse, OAuthProvider } from "types";
 
 const redirectUrl: string = `${import.meta.env.VITE_CLIENT_URL}/login/callback`;
 const authorizeUrl: Record<OAuthProvider, string> = {
-  KAKAO: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${import.meta.env.VITE_OAUTH_KAKAO_KEY}&redirect_uri=${redirectUrl}/kakao`,
-  NAVER: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${import.meta.env.VITE_OAUTH_NAVER_KEY}&redirect_uri=${redirectUrl}/naver`,
+  KAKAO: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${
+    import.meta.env.VITE_OAUTH_KAKAO_KEY
+  }&redirect_uri=${redirectUrl}/kakao`,
+  NAVER: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+    import.meta.env.VITE_OAUTH_NAVER_KEY
+  }&redirect_uri=${redirectUrl}/naver`,
 };
 
 /**
@@ -12,7 +16,8 @@ const authorizeUrl: Record<OAuthProvider, string> = {
  * @param provider
  */
 export const requestOAuthLogin = (provider: OAuthProvider) => {
-  location.href = authorizeUrl[provider];
+  //location.href = authorizeUrl[provider];
+  window.open(authorizeUrl[provider], "_blank");
 };
 
 /**
